06f0584d2df2e2d818a8be93f7f2bc7645ea73c1
twista13 <hemeljainen@hotmail.com>
2016-10-03 12:38:00 +0300

    Hello World

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by twista on 30.09.16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
